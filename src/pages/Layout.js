a551de5702119efed884560ca3e9905231a864ff
kartikey-mittal <g6.kartikey@gmail.com>
2024-04-07 02:58:47 +0530

    hello

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from "./Home";
-import About from "./About";
+
 import List from "./List";
 const Layout = () => {
   return (
@@ -10,7 +10,7 @@ const Layout = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/a" element={<About />} />
+         
           <Route path="/l" element={<List />} />
         </Routes>
       </Router>
